3d2533becb56cb30a223748ddd1ad508d43a239a
GitHub User <88586718+aanzal@users.noreply.github.com>
2021-12-01 15:23:02 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/aanzal/ThePro.git /ThePro
+  git clone https://github.com/aanzal/Millie.git /Millie
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /ThePro
